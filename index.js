5957ce72dbc6fbfbbf59bbabd86ff4a60dd8f40b
Yhucee14 <uchechukwu7814@gmail.com>
2023-09-07 07:41:20 -0700

    added time functionality

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// document.body.innerHTML = "no mercy for zuri";
+function updateDateTime() {
+    const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+    const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+
+    const now = new Date();
+    const utcTimeMilliseconds = now.getTime(); 
+    const dayOfWeek = now.toLocaleDateString(undefined, { weekday: 'long' });
+
+    const formattedUtcTime = new Date(utcTimeMilliseconds);
+
+    const hours = formattedUtcTime.getUTCHours().toString().padStart(2, '0');
+    const minutes = formattedUtcTime.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = formattedUtcTime.getUTCSeconds().toString().padStart(2, '0');
+
+
+    utcTimeElement.textContent = `UTC Time: ${hours}:${minutes}:${seconds}`;
+    dayOfWeekElement.textContent = `Day of the Week: ${dayOfWeek}`;
+}
+
+setInterval(updateDateTime, 1000);
+
+updateDateTime();
