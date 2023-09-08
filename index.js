1cd320725d82f5e63d8f436f8ca7f16aa67bc4f1
Yhucee14 <uchechukwu7814@gmail.com>
2023-09-07 23:49:33 -0700

    more changes

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ function updateDateTime() {
     const utcTimeMilliseconds = now.getTime(); 
     const dayOfWeek = now.toLocaleDateString(undefined, { weekday: 'long' });
 
-    // const formattedUtcTime = new Date(utcTimeMilliseconds);
+    const formattedUtcTime = new Date(utcTimeMilliseconds);
 
-    // const hours = formattedUtcTime.getUTCHours().toString().padStart(2, '0');
-    // const minutes = formattedUtcTime.getUTCMinutes().toString().padStart(2, '0');
-    // const seconds = formattedUtcTime.getUTCSeconds().toString().padStart(2, '0');
+    /
+    const hours = formattedUtcTime.getUTCHours().toString().padStart(2, '0');
+    const minutes = formattedUtcTime.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = formattedUtcTime.getUTCSeconds().toString().padStart(2, '0');
 
-
-    utcTimeElement.textContent = `UTC Time: ${utcTimeMilliseconds}`;
+    utcTimeElement.textContent = `UTC Time: ${hours}:${minutes}:${seconds}`;
     dayOfWeekElement.textContent = `Day of the Week: ${dayOfWeek}`;
 }
 
+
 setInterval(updateDateTime, 1000);
+
 
 updateDateTime();
